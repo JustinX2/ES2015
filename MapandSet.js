66e6448f29590ee333189de323b55a4af82370eb
JustinX2 <129993011+JustinX2@users.noreply.github.com>
2023-05-21 18:52:15 -0700

    Update MapandSet.js

--- MapandSet.js
+++ MapandSet.js
@@ -1,11 +1,9 @@
 /*1. ([1,1,2,2,3,4]) returns a new Set with the values 1, 2, 3, and 4. Sets only contain unique values therefore duplicates will be removed.
 
-2. [...new Set("referee")].join("") first creates a Set with unique values from "referee". 
-The spread operator ... will then create an array from this Set. 
-join("") will concatenate these array elements into a string. 
+2. [...new Set("referee")].join("") creates a Set with unique values, and then ... will then create an array. lastly join("") will concatenate array elements into a string. 
 The output will be:"ref"
 
-3.Even though the two arrays [1,2,3] look the same, they are different objects. Therefore they are treated as different keys. 
+3.Even though the two arrays [1,2,3] are the same, Javascript treat them as different objects. 
 for this reason, the output will be:
 
 Map {Array(3) => true, Array(3) => false}
